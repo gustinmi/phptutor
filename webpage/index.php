0c7bd3b63ee2873fcf62386955a9077c1b1e9f45
Mitja Gustin <mitja@popcom.si>
2014-02-12 14:33:10 +0100

    web page added

---
+++ webpage/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Moja stran</title>
+		<link rel="stylesheet" href="mojstil.css" type="text/css" />
+		<script type="text/javascript" src="mojskript.js" />
+	</head>
+	<body onload="sayHi()">
+
+	<p>
+		Hello from PHP5.
+	</p>
+
+	<?php
+
+		echo "Hi from php";
+
+	?>
+
+	</body>
+</html>
