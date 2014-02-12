08d0af7116147737d73749a7296ef94d5b13215c
Mitja Gustin <mitja@popcom.si>
2014-02-12 14:35:56 +0100

    image added

--- webpage/index.php
+++ webpage/index.php
@@ -1,22 +1,30 @@
 <!DOCTYPE html>
 <html>
+
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>Moja stran</title>
 		<link rel="stylesheet" href="mojstil.css" type="text/css" />
-		<script type="text/javascript" src="mojskript.js" />
+		
 	</head>
+
 	<body onload="sayHi()">
 
-	<p>
-		Hello from PHP5.
-	</p>
+		<div> Some box </div>
 
-	<?php
+		<p>
+			Some text.
+		</p>
 
-		echo "Hi from php";
+		<img src="https://dl.dropboxusercontent.com/u/39195621/best_nav_win.GIF" width="835px" height="955px" >
 
-	?>
+		<?php
 
+			echo "Hi from php";
+
+		?>
+
+		<script type="text/javascript" src="mojskript.js" />
+		
 	</body>
 </html>
