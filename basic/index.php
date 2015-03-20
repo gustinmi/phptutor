0d0b516d8fc291dab1e5e2a377f06fc3a9b67b14
Mitja Guštin <gustinmi@gmail.com>
2015-03-20 18:28:33 +0100

    Update index.php

--- basic/index.php
+++ basic/index.php
@@ -1,18 +1,11 @@
+<?php
+
+require_once("settings.php");
+
+?>
 <!DOCTYPE html> <html> <head> <title>Prva stran</title> </head> <body>
 <?php
 
-// polja
-$dneviTeden = array("Pon", "Tor", "Sre");
-$velikost = count($dneviTeden);
-
-$barva = "red";
-
-$vrstaHtml = "<div style=\"background-color: %s \"> %s </div>";
-
-// pozicija; pogojZa izstop; kako ga povečujemo
-for ($i = 0; $i < $velikost; $i = $i + 1) {
-	echo sprintf($vrstaHtml, $barva, $dneviTeden[$i]);
-}
 
 ?>
 </body>
